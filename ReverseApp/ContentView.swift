51fc0c914247b900d39013b358961ff292bc2f09
Melody Li <zli@lcs.on.ca>
2024-05-30 09:15:48 -0400

    Initial Commit

---
+++ ReverseApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReverseApp
+//
+//  Created by 利梓萌 on 2024-05-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
